a69b2edb2f5ab681a463861b3ce42e585197feea
Ross Moir <Ross.Moir@blend360.com>
2025-01-20 16:50:08 +0000

    Refactors code and adds conjured items

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,46 +1,64 @@
 # -*- coding: utf-8 -*-
+BETTER_AGED_NON_EXPIRE: list = ["Aged Brie"]
+BETTER_AGED_EXPIRE: list = ["Backstage passes to a TAFKAL80ETC concert"]
+LEGENDARY_ITEMS: list = ["Sulfuras, Hand of Ragnaros"]
+CONJURED_ITEMS: list = ["Conjured Mana Cake"]
 
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+MAX_QUALITY: int = 50
+MIN_QUALITY: int = 0
+BASE_QUALITY_CHANGE: int = -1
 
 
-class Item:
-    def __init__(self, name, sell_in, quality):
+class Item(object):
+    def __init__(self, name, sell_in, quality) -> None:
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class GildedRose(object):
+
+    def __init__(self, items: list[Item]) -> None:
+        self.items = items
+
+    @staticmethod
+    def apply_quality_change(item, quality_change: int) -> None:
+        item.quality = max(
+            MIN_QUALITY,
+            min(MAX_QUALITY, item.quality + quality_change),
+        )
+
+    @staticmethod
+    def update_item_sell_in(item: Item) -> None:
+        item.sell_in -= 1
+
+    def update_item_quality(self, item: Item) -> None:
+        depreciation_multiplier: int = 2 if item.name in CONJURED_ITEMS else 1
+
+        if item.name in BETTER_AGED_NON_EXPIRE:
+            self.apply_quality_change(item, 1)
+        elif item.name in BETTER_AGED_EXPIRE:
+            if item.sell_in <= 0:
+                self.apply_quality_change(item, MIN_QUALITY - MAX_QUALITY)
+            elif item.sell_in <= 5:
+                self.apply_quality_change(item, 3)
+            elif item.sell_in <= 10:
+                self.apply_quality_change(item, 2)
+            else:
+                self.apply_quality_change(item, 1)
+        else:
+            if item.sell_in <= 0:
+                depreciation_multiplier *= 2
+            self.apply_quality_change(
+                item, BASE_QUALITY_CHANGE * depreciation_multiplier
+            )
+
+    def update_quality(self) -> None:
+        for item in self.items:
+            if item.name in LEGENDARY_ITEMS:
+                continue
+            self.update_item_sell_in(item=item)
+            self.update_item_quality(item=item)
