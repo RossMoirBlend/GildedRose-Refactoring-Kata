61c3dbc9a4e3f3156f848ce91aca43e66c19d521
Ross Moir <Ross.Moir@blend360.com>
2025-01-21 09:16:36 +0000

    Update to super class implementation

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-BETTER_AGED_NON_EXPIRE: list = ["Aged Brie"]
-BETTER_AGED_EXPIRE: list = ["Backstage passes to a TAFKAL80ETC concert"]
-LEGENDARY_ITEMS: list = ["Sulfuras, Hand of Ragnaros"]
-CONJURED_ITEMS: list = ["Conjured Mana Cake"]
-
 MAX_QUALITY: int = 50
 MIN_QUALITY: int = 0
 BASE_QUALITY_CHANGE: int = -1
@@ -19,46 +14,70 @@ class Item(object):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class BasicItem(Item):
+    def __init__(self, name, sell_in, quality) -> None:
+        super().__init__(name, sell_in, quality)
 
-    def __init__(self, items: list[Item]) -> None:
-        self.items = items
-
-    @staticmethod
-    def apply_quality_change(item, quality_change: int) -> None:
-        item.quality = max(
+    def apply_quality_change(self, quality_change: int) -> None:
+        self.quality = max(
             MIN_QUALITY,
-            min(MAX_QUALITY, item.quality + quality_change),
+            min(MAX_QUALITY, self.quality + quality_change),
         )
 
-    @staticmethod
-    def update_item_sell_in(item: Item) -> None:
-        item.sell_in -= 1
+    def update_item_sell_in(self) -> None:
+        self.sell_in -= 1
 
-    def update_item_quality(self, item: Item) -> None:
-        depreciation_multiplier: int = 2 if item.name in CONJURED_ITEMS else 1
+    def update_item_quality(self) -> None:
+        depreciation_multiplier = 1
+        if self.sell_in <= 0:
+            depreciation_multiplier *= 2
+        self.apply_quality_change(BASE_QUALITY_CHANGE * depreciation_multiplier)
 
-        if item.name in BETTER_AGED_NON_EXPIRE:
-            self.apply_quality_change(item, 1)
-        elif item.name in BETTER_AGED_EXPIRE:
-            if item.sell_in <= 0:
-                self.apply_quality_change(item, MIN_QUALITY - MAX_QUALITY)
-            elif item.sell_in <= 5:
-                self.apply_quality_change(item, 3)
-            elif item.sell_in <= 10:
-                self.apply_quality_change(item, 2)
-            else:
-                self.apply_quality_change(item, 1)
+
+class LegendaryItem(BasicItem):
+    def __init__(self, name, sell_in, quality) -> None:
+        super().__init__(name, sell_in, quality)
+
+    def update_item_sell_in(self) -> None:
+        return None
+
+    def update_item_quality(self) -> None:
+        return None
+
+
+class ConjuredItem(BasicItem):
+    def __init__(self, name, sell_in, quality) -> None:
+        super().__init__(name, sell_in, quality)
+
+    def update_item_quality(self) -> None:
+        depreciation_multiplier = 2
+        if self.sell_in <= 0:
+            depreciation_multiplier *= 2
+        self.apply_quality_change(BASE_QUALITY_CHANGE * depreciation_multiplier)
+
+
+class AgedItem(BasicItem):
+    def __init__(self, name, sell_in, quality, expires=False) -> None:
+        super().__init__(name, sell_in, quality)
+        self.expires: bool = expires
+
+    def update_item_quality(self) -> None:
+        if not self.expires or self.sell_in > 10:
+            self.apply_quality_change(1)
+        elif self.sell_in <= 0:
+            self.apply_quality_change(MIN_QUALITY - MAX_QUALITY)
+        elif self.sell_in <= 5:
+            self.apply_quality_change(3)
         else:
-            if item.sell_in <= 0:
-                depreciation_multiplier *= 2
-            self.apply_quality_change(
-                item, BASE_QUALITY_CHANGE * depreciation_multiplier
-            )
+            self.apply_quality_change(2)
+
+
+class GildedRose(object):
+
+    def __init__(self, items: list[BasicItem]) -> None:
+        self.items = items
 
     def update_quality(self) -> None:
         for item in self.items:
-            if item.name in LEGENDARY_ITEMS:
-                continue
-            self.update_item_sell_in(item=item)
-            self.update_item_quality(item=item)
+            item.update_item_sell_in()
+            item.update_item_quality()
